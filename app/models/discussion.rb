962f09434fa37e18e4fac507465b6d0667e60f59
Stef Coetzee <stefcoetzee@protonmail.com>
2021-06-06 07:56:57 +0200

    Use to_param for discussion link

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -2,4 +2,9 @@ class Discussion < ApplicationRecord
   belongs_to :user, default: -> { Current.user }
 
   validates :name, presence: true
+
+  def to_param
+    "#{id}-#{name.downcase.to_s[0...100]}".parameterize
+  end
+
 end
